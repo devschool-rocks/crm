49769ee6a557f45dce57862139eb09959a845d37
Joe Sak <joe@joesak.com>
2016-03-25 17:54:47 -0600

    Allow params for student

--- app/admin/student.rb
+++ app/admin/student.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register Student do
   menu priority: 2
-  permit_params :name, :birthday, :enrolled_on, :instructor_id, :size_id, :gender_id
+  permit_params :first_name, :last_name, :email, :slack_name,
+    :birthday, :enrolled_on, :instructor_id, :size_id, :gender_id
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
