1dddb46338fa5506a67a3ad254a00766c96d3fd8
Joe Sak <joe@joesak.com>
2016-03-26 10:53:36 -0600

    Customize the leads form

--- app/admin/lead.rb
+++ app/admin/lead.rb
@@ -3,6 +3,8 @@ ActiveAdmin.register Lead do
 
   permit_params :name, :email, :source
 
+  form partial: 'form'
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
