f8fff5a548658a71ddcfc1e755bfc79cb533dcf1
Jim OKelly <thatrubylove@gmail.com>
2016-03-25 22:02:50 -0600

    leads savable now (protected attributes whitelisted)

--- app/admin/lead.rb
+++ app/admin/lead.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register Lead do
   menu priority: 2
 
+  permit_params :name, :email, :source
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
